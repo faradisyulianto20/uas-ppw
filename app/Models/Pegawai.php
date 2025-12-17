3d9d7a9522baafbce7acbe68f2683066373960e5
Faradis Yulianto <faradisy20@gmail.com>
2025-12-17 14:26:24 +0700

    feat: implement dynamic dashboard charts from database

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pegawai extends Model
 {
-    use SoftDeletes;
+     use SoftDeletes;
 
     protected $table = 'pegawai';
 
     protected $dates = ['deleted_at'];
 
-    public function pekerjaan()
+    public function pekerjaan(): BelongsTo
     {
-        return $this->belongsTo(Pekerjaan::class);
+        return $this->belongsTo(Pekerjaan::class, 'pekerjaan_id');
     }
 }
